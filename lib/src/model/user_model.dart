e7a2d21f1aab8c24261256e89880d01ff6156f00
Emmanuel <emmanuelugwueze6@gmail.com>
2023-01-18 04:53:30 +0100

    user model

---
+++ lib/src/model/user_model.dart
@@ -0,0 +1,25 @@
+class UserModel{
+  String? name;
+  String? mobile;
+  String? userId;
+  String? email;
+  String? regNo;
+
+  UserModel({this.regNo,this.name,this.email,this.mobile,this.userId});
+
+  UserModel.fromJson(Map<String, dynamic> data){
+    name = data['name'];
+    mobile = data['mobile'];
+    email = data['email'];
+    regNo= data['regNo'];
+  }
+  toJson(){
+    Map<String,dynamic> data={};
+    data['regNo']= regNo;
+    data['mobile']= mobile;
+    data['email'] = email;
+    data['name'] = name;
+    return data;
+
+  }
+}
