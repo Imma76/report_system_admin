10c3aaf11fb83ebca3ce31df352037965019511e
Emmanuel <emmanuelugwueze6@gmail.com>
2023-01-18 04:06:22 +0100

    app themes

--- lib/src/controllers/theme_controller.dart
+++ lib/src/controllers/theme_controller.dart
@@ -1,7 +1,56 @@
 
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:report_system_admin/src/utils/constants.dart';
 class ThemeController extends ChangeNotifier{
   final textStyle = GoogleFonts.lora(color: primaryColor);
+
+  bool isDark = false;
+  final lightTheme = ThemeData(
+      floatingActionButtonTheme: FloatingActionButtonThemeData(
+        foregroundColor: primaryColor,
+      ),
+// Define the default brightness and colors.
+      brightness: Brightness.light,
+      primaryColor: primaryColor,
+
+
+// Define the default font family.
+      fontFamily: 'Lora',
+      accentColor: primaryColor,
+      //hintColor: primaryColor,
+
+// Define the default `TextTheme`. Use this to specify the default
+// text styling for headlines, titles, bodies of text, and more.
+      textTheme: const TextTheme(
+        headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+        headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+        bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Lora'),
+      ));
+
+  final darkTheme = ThemeData(
+      backgroundColor: Colors.black,
+// Define the default brightness and colors.
+      brightness: Brightness.dark,
+      primaryColor: Colors.white,
+
+// Define the default font family.
+      fontFamily: 'Georgia',
+
+// Define the default `TextTheme`. Use this to specify the default
+// text styling for headlines, titles, bodies of text, and more.
+      textTheme: const TextTheme(
+        headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+        headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+        bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+      ));
+
+  ThemeData get  appTheme => lightTheme;
+  void changeAppTheme(){
+    isDark = !isDark;
+    notifyListeners();
+
+  }
+
 }
