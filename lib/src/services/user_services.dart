a02fdb23dfa0585acfc24912c73595ca358b9f7f
Emmanuel <emmanuelugwueze6@gmail.com>
2023-01-19 11:40:28 +0100

    user login service && user controller

--- lib/src/services/user_services.dart
+++ lib/src/services/user_services.dart
@@ -1,3 +1,15 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
 class UserService{
 
+  Future<User?> loginAdmin({required String email,required  String password})async{
+    try{
+      final response = await FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password: password);
+      return response.user;
+    }catch(e){
+      print(e.toString());
+      throw e.toString();
+    }
+
+  }
 }
