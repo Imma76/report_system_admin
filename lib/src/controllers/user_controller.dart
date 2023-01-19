0756dbb3b171adfdafae6ee1f14b286345e68db9
Emmanuel <emmanuelugwueze6@gmail.com>
2023-01-19 12:15:48 +0100

    route change after login

--- lib/src/controllers/user_controller.dart
+++ lib/src/controllers/user_controller.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:report_system_admin/main.dart';
 import 'package:report_system_admin/src/services/user_services.dart';
+import 'package:report_system_admin/src/views/desktop_view/home_page.dart';
+
+import '../utils/reusable_widgets.dart';
 
 
 class  UserController extends ChangeNotifier{
@@ -15,11 +19,14 @@ class  UserController extends ChangeNotifier{
     notifyListeners();
     final user = await userService.loginAdmin(email: emailController.text.trim(), password: passwordController.text.trim());
 
+    showToast('signed in successfully');
     // if(user!=null){
     //
     // }
     loading=false;
     notifyListeners();
+    Navigator.pushNamed(navigatorKey!.currentContext!, DesktopHomePage.route) ;
+
    // Navigator.pus
 
   }
