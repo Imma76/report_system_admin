5171f83d84e0cb665628f0290bfdbfd81494c624
Emmanuel <emmanuelugwueze6@gmail.com>
2023-01-19 12:23:12 +0100

    injected firebase auth error codes

--- lib/src/services/user_services.dart
+++ lib/src/services/user_services.dart
@@ -1,11 +1,15 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
+import '../utils/error_codess.dart';
+
 class UserService{
 
   Future<User?> loginAdmin({required String email,required  String password})async{
     try{
       final response = await FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password: password);
       return response.user;
+    }on FirebaseAuthException catch(e){
+       ErrorCodes.getFirebaseErrorMessage(e);
     }catch(e){
       print(e.toString());
       throw e.toString();
